e69fd78ab17830fd524cbaba7be85ca719c6237f
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2024-12-12 08:55:10 +1100

    generate sitemap

---
+++ scripts/generate-sitemap.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+const path = require('path');
+
+// Read TOC.json
+const toc = JSON.parse(fs.readFileSync('toc.json', 'utf8'));
+
+// Base URL of the site
+const baseUrl = 'https://haskell4.fun';
+
+// Static routes
+const staticRoutes = [
+    '/',
+    '/about',
+    '/labs',
+    '/read'
+];
+
+// Generate XML entries for static routes
+const staticXml = staticRoutes
+    .map(route => `
+    <url>
+        <loc>${baseUrl}${route}</loc>
+        <changefreq>weekly</changefreq>
+        <priority>${route === '/' ? '1.0' : '0.8'}</priority>
+    </url>`)
+    .join('');
+
+// Generate XML entries for articles
+const articlesXml = toc.articles
+    .map(article => `
+    <url>
+        <loc>${baseUrl}/read/${article.slug}</loc>
+        <changefreq>weekly</changefreq>
+        <priority>0.7</priority>
+    </url>`)
+    .join('');
+
+// Generate XML entries for labs
+const labsXml = fs.readdirSync('articles/labs')
+    .filter(file => file.endsWith('.md'))
+    .map(file => `
+    <url>
+        <loc>${baseUrl}/labs/${file.replace('.md', '')}</loc>
+        <changefreq>weekly</changefreq>
+        <priority>0.7</priority>
+    </url>`)
+    .join('');
+
+// Combine all entries into final sitemap
+const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${staticXml}
+    ${articlesXml}
+    ${labsXml}
+</urlset>`;
+
+// Write sitemap to file
+fs.writeFileSync('sitemap.xml', sitemap);
+console.log('Sitemap generated successfully!'); 
